e4a6a3f106ab2cb11e1a11d27a9f86adba66b2e1
Ocead <johannes.milczewski@ocead.de>
2022-12-01 18:50:07 +0100

    Added point class and object class documentation

--- include/plang/corpus/plot/object.hpp
+++ include/plang/corpus/plot/object.hpp
@@ -2,8 +2,120 @@
 // Created by Johannes on 16.08.2022.
 //
 
-/// \page lang_plot_object Objects
-
+/** \page lang_plot_object Objects
+ *
+ * [TOC]
+ *
+ * # Object classes
+ *
+ * Object classes structure the meaning of objects in points (sentences). Each point class possesses its own set of
+ * object classes. While most point classes in the standard corpus get by with a single or even no required object,
+ * some statements are more complex than that, for example:
+ *
+ * ```
+ * (.character[]) .character.behaviour.speech
+ *           what
+ *           ?to : (.character[]);
+ * ```
+ *
+ * This point class may be used for for a line of verbal dialogue. Its object classes are what is said (`what`) and to
+ * whom (`?to`).
+ *
+ * ## Declaration
+ *
+ * Object classes are declared together with point classes. Their declaration follows the path and optional decoration
+ * of the point class.
+ *
+ * The simplest form of declaring object classes is simply stating their names in sequence:
+ *
+ * ```
+ * .character.behaviour.speech ?what ?to;
+ * ```
+ *
+ * Each object class begins with a question mark (`?`) followed by a name unique to the scope of the point class,
+ * meaning multiple point classes may have an object class named `?what`.
+ * Even if there are multiple object classes, they are not connected with commas (`,`) in this case. This is done to
+ * mimic the appearance of a sentence in natural language.
+ *
+ * The first object class in the declaration may be marked as the point classes default object class by omitting the
+ * leading question mark (`?`), like so:
+ *
+ * ```
+ * .character.behaviour.speech what ?to;
+ * ```
+ *
+ * The name of the default object class may be omitted in point declarations, again mimicking natural language as well
+ * as shortening the syntax.
+ *
+ * Each object class may also be appended with a definition. These can be used to either explain the usage of the
+ * object class, or to hint to the kinds of entries that should be used with it.
+ *
+ * To define an object class, append it with a colon (`:`) followed by any of the following in parentheses (`()`):
+ *
+ * <ul>
+ * <li>
+ * a string literal (e.g.: `"This is a comment"`)<br/>
+ * This acts like a comment without any impact on the parser logic
+ * </li>
+ * <li>
+ * a like hint (e.g.: `l""`)<br/>
+ * Meaning a text matching the hint as in an SQLite `LIKE` expression should be used as object.<br/>
+ * See [this link](https://www.w3schools.com/sql/sql_like.asp) for further instructions.
+ * </li>
+ * <li>
+ * a glob hint (e.g.: `g""`)<br/>
+ * Meaning a text matching the hint as a glob pattern should be used as object.<br/>
+ * See [this link](https://man7.org/linux/man-pages/man7/glob.7.html) for further instructions.
+ * </li>
+ * <li>
+ * a regex hint (e.g.: `r"^(yes|no)$"`)<br/>
+ * Meaning a text matching the hint as a regular expression should be used as object
+ * </li>
+ * <li>
+ * a match hint (e.g.: `m"yes OR no"`)<br/>
+ * Meaning a text matching the hint as aSQLite FTS match filter should be used as object
+ * </li>
+ * <li>
+ * a symbol class reference (e.g.: `.symbol.class[]`)<br/>
+ * Meaning a symbol of this class or the class itself should be used as object
+ * </li>
+ * <li>
+ * a recursive symbol class reference (e.g.: `.symbol.class[]...`)<br/>
+ * Meaning a symbol of this class or its children or the classes themselves should be used as object
+ * </li>
+ * <li>
+ * a point class reference (e.g.: `.point.class?:`)<br/>
+ * Meaning a point of this class should be used as object
+ * </li>
+ * <li>
+ * a recursive point class reference (e.g.: `.point.class?:...`)<br/>
+ * Meaning a point of this class or its children should be used as object
+ * </li>
+ * </ul>
+ *
+ * Multiple literals and references may be concatenated with the comma (`,`) operator.
+ * An object only has to satisfy one of the definitions (comments excluded).
+ *
+ * Normally, a point can have multiple entries as one of its objects simultaneously, like addressing two characters
+ * in `.character.behaviour.speech`. To enforce that only a single entry may be the object, object classes can be to
+ * marked as singleton be prepending them with an exclamation mark (`!`), like so:
+ *
+ * ```
+ * (.character[]) ! .character.identity.birthday ! is;
+ * ```
+ *
+ * ## Reference
+ *
+ * An object class may be referenced by its point classes path followed by a question mark (`?`) and the name of the
+ * object class. The same rules for abbreviation as with symbol classes apply.
+ *
+ * ```
+ * .object.optical.color?is
+ * object.optical.color?is
+ * optical.color?is
+ * color?is
+ * ```
+ */
 #ifndef PLANG_OBJECT_HPP
 #define PLANG_OBJECT_HPP
 
--- include/plang/corpus/plot/point.hpp
+++ include/plang/corpus/plot/point.hpp
@@ -2,7 +2,80 @@
 // Created by Johannes on 16.08.2022.
 //
 
-/// \page lang_plot_point Points
+/** \page lang_plot_point Points
+ *
+ * [TOC]
+ *
+ * # Point classes
+ *
+ * Point classes structure the kinds of statements you can make about symbols. The work as tags on or relations between
+ * points. For example
+ *
+ *  + the point classes under `.character.relationship?:...` contain the kinds of relationships character may have
+ *  + the point class `.world.geography.leads?:` may be used for building maps by making nets between adjacent places
+ *  + the point class `.object.optical.color?:` defines the color of an object
+ *
+ *  As with symbol classes, each point class has a unique path and always denote the same kind or statement.
+ *
+ *  ## Declaration
+ *
+ *  Within declarations, point class dont have an identifying operator. Rather they already mimic the structure of
+ *  sentences that can be formed with them. In the default configuration point classes (and later points) are declared
+ *  in a subject-verb-object order. Point classes dont have concrete subjects, so the shortest possible point class
+ *  declarations like this:
+ *
+ *  ```
+ *  .character.lives _;
+ *  ```
+ *
+ *  Here `.character.lives` is the unique path of the point class and the following `_` is it's default object class.
+ *  This point class may be used to form statements about which characters are alive. This statement does not require
+ *  additional objects. This syntax, using `_` as the name for a points class obsolete default object class, is
+ *  convention used in the standard corpus.
+ *
+ *  Decorations may be applied to point classes like with symbol classes:
+ *
+ *  ```
+ *  .character.lives (-4, "Denotes that a character lives") _;
+ *  ```
+ *
+ *  To specify which symbols may be the subject of a point of a point class, hints may be applied to the declaration:
+ *
+ *  ```
+ *  (.character[]) .character.lives _;
+ *  ```
+ *
+ *  If a point class is hinted, the parser will favor symbols of matching classes when looking up matching candidates
+ *  in point declarations. Thus, the class path of the subject symbol may be shortened or omitted.
+ *  Multiple subject hints may be concatenated with the comma (`,`) operator. Recursive hints are denoted with an
+ *  ellipsis (`...`).
+ *
+ *  Point classes may be marked as singletons. This means, that at any time, only one point of this class may exist for
+ *  each symbol. This is done by adding an exclamation mark (`!`) before the point classes path:
+ *
+ *  ```
+ *  (.character[]) ! .character.lives _;
+ *  ```
+ *
+ *  Within the story context, declaring a new singleton point will override earlier points instead of adding to them.
+ *
+ *  A point declaration also includes declarations for a points object classes. Their syntax is explained
+ *  [here](@ref lang_plot_object).
+ *
+ *  ## Reference
+ *
+ *  Referencing a point class on its own is done by appending the path of the point class with a question mark and a
+ *  colon (`?:`). The same rules for abbreviation as with symbol classes apply.
+ *
+ *  ```
+ *  .world.geography.natural.climate.weather?:
+ *  world.geography.natural.climate.weather?:
+ *  geography.natural.climate.weather?:
+ *  natural.climate.weather?:
+ *  climate.weather?:
+ *  weather?:
+ *  ```
+ */
 
 #ifndef PLANG_POINT_HPP
 #define PLANG_POINT_HPP
